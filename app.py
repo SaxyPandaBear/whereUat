f1646133f9ec690f397f097bbfa37d64382d3e2b
Ty <traffens@gmu.edu>
2017-12-02 15:38:53 -0500

    working

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, Response
+import json
+
+import requests
+import urllib2
+
 
 class MyLocation:
     def __init__(self, **kwargs):
         self.latitude = kwargs.get('latitude')
         self.longitude = kwargs.get('longitude')
+
+    def __str__(self):
+        return '( %s,%s )' % (self.latitude, self.longitude)
+
+    def __repr__(self):
+        return self.__str__()
 
 app = Flask(__name__)
 
@@ -15,10 +26,14 @@ def get_locations():
 # read geolocation data from the file, and returns a
 # list of MyLocation objects created from data in the file
 def read_locations():
-    with open('file', 'r') as file:
-        locs = file.read().splitlines()
-    return locs
+    locs = []
+    for line in urllib2.urlopen('http://108.44.193.253:12000/IP.txt'):
+        geo = line.split(",")
+        locs.append(MyLocation(latitude=geo[0], longitude=geo[1]))
+
+    print str(locs)
+
+    return Response(json.dumps(locs), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
