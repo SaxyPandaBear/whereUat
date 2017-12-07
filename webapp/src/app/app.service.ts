4888fc6583ad66524c94d798328797eaa8a43d59
Andrew Huynh <andrew.huynh@bti360.com>
2017-12-06 23:44:20 -0500

    added placeholder return values and started new endpoint

--- webapp/src/app/app.service.ts
+++ webapp/src/app/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 import {of as observableOf} from 'rxjs/observable/of'; // for mocking responses
 import { HttpClient } from "@angular/common/http";
-import { GeolocationPayload } from "./models";
+import { GeolocationPayload, Geolocation, IpInfo, IpInfoPayload } from "./models";
 
 @Injectable()
 export class AppService {
@@ -11,7 +11,33 @@ export class AppService {
 
     constructor(private http: HttpClient) {}
 
-    getLocations(): Observable<GeolocationPayload> {
-        return this.http.get<GeolocationPayload>('http://localhost:5000/location');
+    public getLocations(): Observable<GeolocationPayload> {
+        return observableOf({locs: this.getStaticLocation()});
+        // return this.http.get<GeolocationPayload>('http://localhost:5000/location');
+    }
+
+    public getIpInfo(): Observable<IpInfoPayload> {
+        return observableOf({ips: this.getStaticIpInfo()});
+        // return this.http.get<IpInfoPayload>('http://localhost:5000/ip');
+    }
+
+    private getStaticLocation(): Geolocation[] {
+        return [
+            {latitude: 38.831554, longitude: -77.312089}
+        ];
+    }
+
+    private getStaticIpInfo(): IpInfo[] {
+        return [
+            {
+                ip:'1.1.1.1',
+                hostname:'www.gmu.edu',
+                city:'Fairfax',
+                country:'US', 
+                latitude:38.831554, 
+                longitude:-77.312089, 
+                freq:1
+            }
+        ];
     }
 }
